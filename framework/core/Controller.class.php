43efea3a4ddd6765d97f2ba00771e98bd7a12ee9
Denis Souza <denis@merchannew.com.br>
2017-08-15 08:56:45 -0300

    Fixed.

--- framework/core/Controller.class.php
+++ framework/core/Controller.class.php
@@ -4,8 +4,6 @@
 
 class Controller{
 
-  // Base Controller has a property called $loader, it is an instance of Loader class(introduced later)
-
   protected $loader;
 
   public function __construct(){
